04bbd4036bbc4794ed5eaa915ea099bbe1494cb9
DumakIt <odumak4411@gmail.com>
2023-03-09 10:53:57 +0900

    분기배포

--- next.config.js
+++ next.config.js
@@ -2,6 +2,16 @@
 const nextConfig = {
   reactStrictMode: true,
   trailingSlash: true,
+  generateBuildId: () => "class-build-project",
+
+  // 아래 주소들만 out폴더에 만들어줘! (getServerSideProps 있는 페이지는 제외)
+  exportPathMap: () => {
+    return {
+      "/": { page: "/" },
+      "/board": { page: "/board" },
+      "/404": { page: "/404" },
+    };
+  },
 };
 
 module.exports = nextConfig;
